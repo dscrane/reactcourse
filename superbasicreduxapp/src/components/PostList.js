60a66ddcc9f8b6b84b4e4ba3df6413ffa580b550
dscrane <daegancrane@gmail.com>
2020-08-17 09:43:45 -0400

    adding the first redux async request with redux-thunk

--- superbasicreduxapp/src/components/PostList.js
+++ superbasicreduxapp/src/components/PostList.js
@@ -3,13 +3,19 @@ import { connect } from 'react-redux';
 import { fetchPosts} from "../actions";
 
 class PostList extends Component {
+
   componentDidMount() {
     this.props.fetchPosts();
   }
 
   render() {
+    console.log(this.props.posts)
     return <div>PostList</div>
   }
 }
 
-export default connect(null, { fetchPosts })(PostList);
+const mapStateToProps = (state) => {
+  return { posts: state.posts };
+}
+
+export default connect(mapStateToProps, { fetchPosts })(PostList);
