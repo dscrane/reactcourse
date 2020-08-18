c41f5e1c37d206ace7561ea18ba0ee723f355c70
dscrane <daegancrane@gmail.com>
2020-08-18 07:15:53 -0400

    finshed the basic redux app

--- superbasicreduxapp/src/actions/index.js
+++ superbasicreduxapp/src/actions/index.js
@@ -9,11 +9,31 @@ export const fetchPosts = () => {
   }
 }
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`)
 
   dispatch({ type: 'FETCH_USER', payload: response.data });
-})
+}
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  await dispatch(fetchPosts());
+  _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
+
+}
+
+/*
+    A way to use one time memoization to slow the number of requests but this means
+    we will only ever be able to request an id a single time even if the data we
+    are searching for has changed which is not ideal for most use cases
+*/
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//   const response = await jsonPlaceholder.get(`/users/${id}`)
+//
+//   dispatch({ type: 'FETCH_USER', payload: response.data });
+// })
