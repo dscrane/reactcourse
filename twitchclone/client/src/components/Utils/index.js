bc44d9e44e47738241e844b25b74bd74662af766
dscrane <daegancrane@gmail.com>
2020-08-21 13:28:36 -0400

    added the delete functionality and modal confirmation

--- twitchclone/client/src/components/Utils/index.js
+++ twitchclone/client/src/components/Utils/index.js
@@ -1,2 +1,3 @@
-export * from './Header'
+export * from './Header';
 export * from './GoogleAuth';
+export * from './Modal';
--- twitchclone/client/src/components/streams/StreamDelete.js
+++ twitchclone/client/src/components/streams/StreamDelete.js
@@ -1,26 +1,50 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
 import { connect } from 'react-redux';
+import { Modal } from '../Utils';
 
-import { fetchStream } from '../../redux/actions';
+import history from "../../history";
+import { fetchStream, deleteStream } from '../../redux/actions';
+
 
 class StreamDelete extends Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id);
   }
 
+  renderActions() {
+    const { id } = this.props.match.params;
+    return (
+      <>
+        <button onClick={() => this.props.deleteStream(id)} className='ui button negative'>Delete</button>
+        <Link to={'/'} className='ui button'>Cancel</Link>
+      </>
+    )
+  }
+
+  renderContent() {
+    if (!this.props.stream) {
+      return 'Are you sure you want to delete this stream?'
+    }
+    return `Are you sure you want to delete: ${this.props.stream.title}`
+  }
+
   render() {
     return (
-      <div>
-        StreamList
-      </div>
+      <Modal
+        title='Delete Stream'
+        content={this.renderContent()}
+        actions={this.renderActions()}
+        onDismiss={() => history.push('/')}
+      />
     )
   };
 }
 
 const mapStateToProps = (state, ownProps) => {
   return {
-    stream: this.state.streams[ownProps.match.params.id]
+    stream: state.streams[ownProps.match.params.id]
   }
-}
+};
 
-export default connect(mapStateToProps, { fetchStream })(StreamDelete)
+export default connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete);
