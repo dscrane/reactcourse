6f53d35ed25508f625ec3e6720f6222fbd58d90c
dscrane <daegancrane@gmail.com>
2020-08-23 08:33:12 -0400

    finished the context based app

--- languageapp/src/components/Button.js
+++ languageapp/src/components/Button.js
@@ -3,11 +3,17 @@ import LanguageContext from '../contexts/LanguageContext';
 import ColorContext from "../contexts/ColorContext";
 
 export class Button extends Component {
+  static contextType = LanguageContext;
+
+  renderSubmit(language){
+    return language === 'english' ? 'Submit' : 'Voorleggen'
+  }
+
   renderButton(color) {
     return(
       <button className={`ui button ${color}`}>
         <LanguageContext>
-          {(value) => value === 'english' ? 'Submit' : 'Voorleggen'}
+          {({ language }) => this.renderSubmit(language)}
         </LanguageContext>
       </button>
     )
